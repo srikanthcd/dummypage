46f2f7ab6243c192e0ce9aeb5826d0e34bb5b2ef
srikanthcd <srikanthpathigulla@gmail.com>
2019-06-27 01:59:32 +0530

    my_first_attempt

---
+++ blog-frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
